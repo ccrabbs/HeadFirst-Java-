5da0028d9b271164efc0e5e2480aeb9502b12bdd
ccrabbs <32720109+ccrabbs@users.noreply.github.com>
2022-06-02 11:00:19 -0700

    Add files via upload

---
+++ IFTest.java
@@ -0,0 +1,12 @@
+public class IFTest {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 4){
+            System.out.println("x must be 4");
+        }
+        else {
+            System.out.println("x is not 4");
+        }
+        System.out.println("This runs no matter what");
+    }
+}
